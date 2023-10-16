3bc91968e0db980e0b2eb89755c405bc8475827b
petrostrak <pit.trak@gmail.com>
2023-10-16 20:07:33 +0300

    add subscribe_returns_a_200_for_valid_form_data

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,13 @@
-use std::net::TcpListener;
+use std::{fmt::format, net::TcpListener};
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletter::run(listener).expect("Failed to bind address.");
+
+    tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[tokio::test]
 async fn health_check_works() {
@@ -15,11 +24,19 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = newsletter::run(listener).expect("Failed to bind address.");
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
 
-    tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
 }
