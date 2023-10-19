d8f76afd87f6a2b51bf7527697afe8f3aab1271a
petrostrak <pit.trak@gmail.com>
2023-10-19 12:48:17 +0300

    reading a configuration file

---
+++ src/configuration.rs
@@ -0,0 +1,27 @@
+use config::{Config, File};
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Initialize our config reader
+    let mut settings = Config::default();
+
+    // Add configuration values from a file named "configuration".
+    settings.merge(File::with_name("configuration"))?;
+
+    // Try to convert the configuration values into Settings type
+    settings.try_into()
+}
