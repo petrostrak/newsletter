0507de5bbbd85e9ae10449c102a0175988fa512b
petrostrak <pit.trak@gmail.com>
2023-10-19 19:22:03 +0300

    actix-web workers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::net::TcpListener;
 
 use newsletter::{configuration::get_configuration, startup::run};
+use sqlx::{Connection, PgConnection};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to localhost.");
-    run(listener)?.await
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres ");
+
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,18 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgConnection;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Register the connection as part of the application state
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
