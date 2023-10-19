865c6e3a259840278fa4cd3ff4b936768562fa64
petrostrak <pit.trak@gmail.com>
2023-10-19 13:15:34 +0300

    connectiong to postgres

--- src/configuration.rs
+++ src/configuration.rs
@@ -15,6 +15,15 @@ pub struct DatabseSettings {
     pub database_name: String,
 }
 
+impl DatabseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // Initialize our config reader
     let mut settings = Config::default();
