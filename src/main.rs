e444501e1b55c7a6f43a06a8f44a38a92a26daa7
petrostrak <pit.trak@gmail.com>
2023-11-06 17:10:58 +0200

    move initialisation to telemetry mod

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,11 @@
 use std::net::TcpListener;
 
-use newsletter::{configuration::get_configuration, startup::run};
+use newsletter::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 use sqlx::PgPool;
-use tracing::{dispatcher::set_global_default, Subscriber};
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, EnvFilter, Registry};
-
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
-
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-}
-
-pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
-    LogTracer::init().expect("Failed to set logger");
-    set_global_default(subscriber.into()).expect("Failed to set subscriber");
-}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
