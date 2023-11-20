13f153827f90d55187a20421fc99cefccd620dcc
petrostrak <pit.trak@gmail.com>
2023-11-20 20:38:01 +0200

    add request_id to requests

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,16 @@
-use std::net::TcpListener;
-
-use actix_web::{dev::Server, web, App, HttpServer};
-use sqlx::PgPool;
-
 use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
+
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Register the connection as part of the application state
