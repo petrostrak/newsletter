761470580deafe2af89e850a7cb4ca990eafc6c3
petrostrak <pit.trak@gmail.com>
2023-11-21 16:48:43 +0200

    connect lazy to db

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,7 @@ async fn main() -> std::io::Result<()> {
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to localhost.");
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres ");
 
     run(listener, connection_pool)?.await
